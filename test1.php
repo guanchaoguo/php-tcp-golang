4640e5f6873556d3f29c643e4fb76a913e293ebf
guanchaoguo <guanchaoguo@qq.com>
2018-11-02 16:50:52 +0800

    no message

--- test1.php
+++ test1.php
@@ -10,30 +10,21 @@ if (!$fp) {
 $header ='www.01happy.com';
 $str = '{"Id":1,"Name":"golang","Message":"message"}';
 $len= strlen($str);
- $data =  pack('A15N1A44',$header,$len,$str);
+$data =  pack('A15N1A44',$header,$len,$str);
  //$data 按照一定格式被打包成二进制数据
- for ($i=0; $i<10;$i++) {
-    fwrite($fp, $data);
- }
+   for ($i=0; $i<10;$i++) {
+      fwrite($fp, $data);
+   }
 
- 
- if (!feof($fp)) {
-  $rs = fread($fp, 1024);
-  //读取远程数据
-  if ($rs) {
-   $len = strlen($rs);
-   //$len 可以获取数据的长度，用以计算content的长度
-   //在这个例子中，content 的长度为 4
- 
-   $format = "A15header/Nlen/A44content";
-   $data = unpack($format, $rs);
- 
-   print_r($data);
-  } else {
-   echo "timeout!";
-  }
- } else {
-  echo "timeout!";
- }
+  $i = 0;
+   while($i <10 ) {
+      $i++;
+      $rs = fread($fp, 63);
+      $format = "A15header/Nlen/A44content";
+      $data = unpack($format, $rs);
+      
+      print_r($data);
+        
+ } 
  fclose($fp);
 }
